f2592be20818d988e17ada877aec9c3ba1d35288
Yasmin Lahnin <yasmin.lahnin@iut-dijon.u-bourgogne.fr>
2025-02-19 10:12:02 +0100

    [Fix] Finition de la refactorisation de simulateur

--- 03.tax-simulator/c#/Tax.Simulator/Simulateur.cs
+++ 03.tax-simulator/c#/Tax.Simulator/Simulateur.cs
@@ -5,91 +5,7 @@ public static class Simulateur
     private static readonly decimal[] TranchesImposition = {10225m, 26070m, 74545m, 160336m}; // Plafonds des tranches
     private static readonly decimal[] TauxImposition = {0.0m, 0.11m, 0.30m, 0.41m, 0.45m}; // Taux correspondants
 
-    public static decimal CalculerImpotsAnnuel(
-        string situationFamiliale,
-        decimal salaireMensuel,
-        decimal salaireMensuelConjoint,
-        int nombreEnfants)
-    {
-        if (situationFamiliale != "Célibataire" && situationFamiliale != "Marié/Pacsé")
-        {
-            throw new ArgumentException("Situation familiale invalide.");
-        }
-
-        if (salaireMensuel <= 0)
-        {
-            throw new ArgumentException("Les salaires doivent être positifs.");
-        }
-
-        if (situationFamiliale == "Marié/Pacsé" && salaireMensuelConjoint < 0)
-        {
-            throw new InvalidDataException("Les salaires doivent être positifs.");
-        }
-
-        if (nombreEnfants < 0)
-        {
-            throw new ArgumentException("Le nombre d'enfants ne peut pas être négatif.");
-        }
-
-        decimal revenuAnnuel;
-        if (situationFamiliale == "Marié/Pacsé")
-        {
-            revenuAnnuel = (salaireMensuel + salaireMensuelConjoint) * 12;
-        }
-        else
-        {
-            revenuAnnuel = salaireMensuel * 12;
-        }
-
-        var baseQuotient = situationFamiliale == "Marié/Pacsé" ? 2 : 1;
-        decimal quotientEnfants = (decimal) Math.PI;
-
-        if (nombreEnfants == 0)
-        {
-            quotientEnfants = 0;
-        }
-        else if (nombreEnfants == 1)
-        {
-            quotientEnfants = 0.5m;
-        }
-        else if (nombreEnfants == 2)
-        {
-            quotientEnfants = 1.0m;
-        }
-        else
-        {
-            quotientEnfants = 1.0m + (nombreEnfants - 2) * 0.5m;
-        }
-
-        var partsFiscales = baseQuotient + quotientEnfants;
-        var revenuImposableParPart = revenuAnnuel / partsFiscales;
-
-        decimal impot = 0;
-        for (var i = 0; i < TranchesImposition.Length; i++)
-        {
-            if (revenuImposableParPart <= TranchesImposition[i])
-            {
-                impot += (revenuImposableParPart - (i > 0 ? TranchesImposition[i - 1] : 0)) * TauxImposition[i];
-                break;
-            }
-            else
-            {
-                impot += (TranchesImposition[i] - (i > 0 ? TranchesImposition[i - 1] : 0)) * TauxImposition[i];
-            }
-        }
-
-        if (revenuImposableParPart > TranchesImposition[^1])
-        {
-            impot += (revenuImposableParPart - TranchesImposition[^1]) * TauxImposition[^1];
-        }
-
-        var impotParPart = impot;
-
-        return Math.Round(impotParPart * partsFiscales, 2);
-        
-    }
-
-    public static decimal calculerImpotAnnuelleBis(string situationFamiliale, decimal salaireMensuel, decimal salaireMensuelConjoint, int nombreEnfants)
+    public static decimal CalculerImpotsAnnuel(string situationFamiliale, decimal salaireMensuel, decimal salaireMensuelConjoint, int nombreEnfants)
     {
         if (situationFamiliale != "Célibataire" && situationFamiliale != "Marié/Pacsé")
         {
@@ -120,28 +36,10 @@ public static class Simulateur
         var partsFiscales = baseQuotient + quotientEnfants;
         var revenuImposableParPart = revenuAnnuel / partsFiscales;
 
-        decimal impot = 0;
-        for (var i = 0; i < TranchesImposition.Length; i++)
-        {
-            if (revenuImposableParPart <= TranchesImposition[i])
-            {
-                impot += (revenuImposableParPart - (i > 0 ? TranchesImposition[i - 1] : 0)) * TauxImposition[i];
-                break;
-            }
-            else
-            {
-                impot += (TranchesImposition[i] - (i > 0 ? TranchesImposition[i - 1] : 0)) * TauxImposition[i];
-            }
-        }
+        decimal impot = Simulateur.impotParPart(revenuImposableParPart);
 
-        if (revenuImposableParPart > TranchesImposition[^1])
-        {
-            impot += (revenuImposableParPart - TranchesImposition[^1]) * TauxImposition[^1];
-        }
 
-        var impotParPart = impot;
-
-        return Math.Round(impotParPart * partsFiscales, 2);
+        return Math.Round(impot * partsFiscales, 2);
     }
 
     private static decimal revenueAnnuel(string situationFamiliale, decimal salaireMensuel, decimal salaireMensuelConjoint)
@@ -206,6 +104,5 @@ public static class Simulateur
         var impotParPart = impot;
         return impotParPart;
         
-        impot = TranchesImposition.Sum()
     }
 }
