aee267d339608047e4d003da55d62b6118baee9a
Yasmin Lahnin <yasmin.lahnin@iut-dijon.u-bourgogne.fr>
2025-02-19 08:42:26 +0100

    [Test][US3] Test ImpotMariePacse3Enfants situation valide

--- 03.tax-simulator/c#/Tax.Simulator.Tests/SimulateurShould.cs
+++ 03.tax-simulator/c#/Tax.Simulator.Tests/SimulateurShould.cs
@@ -7,8 +7,16 @@ public class SimulateurShould
     [Fact]
     public void ImpotCelibataire()
     {
-        Simulateur.CalculerImpotsAnnuel("C�libataire", 2000, 0, 0)
+        Simulateur.CalculerImpotsAnnuel("C�libataire", 2000, 0, 0)
             .Should()
             .Be(1515.25m);
+    }
+    
+    [Fact (DisplayName = "Marié/Pacsé 3 enfants")]
+    public void ImpotMariePacse3Enfants()
+    {
+        Simulateur.CalculerImpotsAnnuel("Marié/Pacsé", 3000, 3000, 3)
+            .Should()
+            .Be(3983.37m);
     }
 }
