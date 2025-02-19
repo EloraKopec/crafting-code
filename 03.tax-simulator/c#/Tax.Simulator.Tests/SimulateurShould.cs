42a32ba73c856146cc343c5d60e02a2df1bb9639
Elora, Enzo, Maloric <ek659175@IQ-146-14>
2025-02-19 09:14:16 +0100

    [Test][US2] Ajout des tests de l'US2

--- 03.tax-simulator/c#/Tax.Simulator.Tests/SimulateurShould.cs
+++ 03.tax-simulator/c#/Tax.Simulator.Tests/SimulateurShould.cs
@@ -10,7 +10,7 @@ public class SimulateurShould
         Simulateur.CalculerImpotsAnnuel("Célibataire", 2000, 0, 0)
             .Should()
             .Be(1515.25m);
-        
+
         Simulateur.CalculerImpotsAnnuel("Célibataire", 1978123.98m, 0, 0)
             .Should()
             .Be(10661178.05m);
@@ -26,50 +26,66 @@ public class SimulateurShould
 
     }
 
-    [Fact (DisplayName = "Marié/Pacsé 3 enfants")]
+    [Fact(DisplayName = "Marié/Pacsé 3 enfants")]
     public void ImpotMariePacse3Enfants()
     {
         Simulateur.CalculerImpotsAnnuel("Marié/Pacsé", 3000, 3000, 3)
             .Should()
             .Be(3983.37m);
-        
+
         Simulateur.CalculerImpotsAnnuel("Marié/Pacsé", 3000, 3000, 3)
             .Should()
             .NotBe(3982.37m);
-        
     }
-    
-    [Fact (DisplayName = "Marié/Pacsé -1 enfant")]
+
+    [Fact(DisplayName = "Marié/Pacsé -1 enfant")]
     public void ImpotMariePacseMoins1Enfant()
     {
-        Action action = () => 
+        Action action = () =>
         Simulateur.CalculerImpotsAnnuel("Marié/Pacsé", 3000, 3000, -1);
-            action.Should()
-            .ThrowExactly<ArgumentException>()
-            .WithMessage("Le nombre d'enfants ne peut pas être négatif.");
+        action.Should()
+        .ThrowExactly<ArgumentException>()
+        .WithMessage("Le nombre d'enfants ne peut pas être négatif.");
     }
-    
-    [Fact (DisplayName = "Situation familial invalide")]
+
+
+    [Fact]
+    public void ImpotMarieShould()
+    {
+        Simulateur.CalculerImpotsAnnuel("Marié/Pacsé", 2000, 2500, 0)
+            .Should()
+            .Be(4043.90m);
+    }
+
+    [Fact]
+    public void ImpotMarieThrow()
+    {
+        Action act = () =>
+            Simulateur.CalculerImpotsAnnuel("Marié/Pacsé", -2500, 2000, 0);
+        act.Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact(DisplayName = "Situation familial invalide")]
     public void ImpotSituationFamilialeInvalide()
     {
-        Action action = () => 
+        Action action = () =>
         Simulateur.CalculerImpotsAnnuel("Divorcé", 3000, 3000, 3);
-            action.Should()
-            .ThrowExactly<ArgumentException>()
-            .WithMessage("Situation familiale invalide.");
-    } 
-    
-    [Fact (DisplayName = "Les salaires doivent être positifs")]
+        action.Should()
+        .ThrowExactly<ArgumentException>()
+        .WithMessage("Situation familiale invalide.");
+    }
+
+    [Fact(DisplayName = "Les salaires doivent être positifs")]
     public void ImpotSalaireNegatif()
     {
-        Action action = () => 
+        Action action = () =>
         Simulateur.CalculerImpotsAnnuel("Marié/Pacsé", -3000, 3000, 3);
-            action.Should()
-            .ThrowExactly<ArgumentException>()
-            .WithMessage("Les salaires doivent être positifs.");
+        action.Should()
+        .ThrowExactly<ArgumentException>()
+        .WithMessage("Les salaires doivent être positifs.");
     }
-    
-    [Fact (DisplayName = "Salaire 2 millions")]
+
+    [Fact(DisplayName = "Salaire 2 millions")]
     public void ImpotSalaire2Millions()
     {
         Simulateur.CalculerImpotsAnnuel("Marié/Pacsé", 2000000, 10000, 3)
@@ -77,3 +93,4 @@ public class SimulateurShould
             .Be(10781579.96m);
     }
 }
+
